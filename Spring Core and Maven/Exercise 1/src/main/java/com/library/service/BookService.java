59e637737aacf480bb89f6df67d3a853983e3567
Nav-S24 <useless0079@gmail.com>
2026-07-01 15:41:50 +0530

    Added Spring Core and Maven Exercise 1

---
+++ Spring Core and Maven/Exercise 1/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,20 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    public BookService() {
+        System.out.println("BookService Bean Created");
+    }
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void showBooks() {
+        bookRepository.displayBooks();
+    }
+}
